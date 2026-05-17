13d1eda82a3c5847b482dbbd6bd70f1b3fcc62e3
Kacaw-X <muhammadnofanfirdaus0@gmail.com>
2026-05-18 04:30:36 +0900

    Pertemuan 2: Templating dan layouting view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,13 +1,31 @@
 <?php
+
 use CodeIgniter\Router\RouteCollection;
+
 /**
-* @var RouteCollection $routes
-*/
-// Route default (halaman beranda)
+ * @var RouteCollection $routes
+ */
+
+// Default route (beranda)
 $routes->get('/', 'Beranda::index');
-// Route halaman tentang
+
+// Halaman tentang
 $routes->get('tentang', 'Beranda::tentang');
-// Route dengan parameter numerik
+
+// Route controller Demo
+$routes->get('demo', 'Demo::index');
+
+// Route dengan parameter angka
 $routes->get('pengguna/(:num)', 'Beranda::pengguna/$1');
-// Route halaman waktu
+
+// Halaman waktu
 $routes->get('waktu', 'Beranda::waktu');
+
+// Route untuk Sistem Informasi Akademik
+$routes->get('akademik', 'Akademik::index');
+
+// Route untuk daftar mata kuliah
+$routes->get('akademik/matkul', 'Akademik::matkul');
+
+// Route untuk nilai mahasiswa dengan parameter NIM
+$routes->get('akademik/nilai/(:segment)', 'Akademik::nilai/$1');
